27b19b78ac202d1fab63bfc959c39beb8f8cd141
KennyGoreham <goreham.kenny2@gmail.com>
2024-02-20 14:47:35 +0000

    Completed Task 8 - patch-article-by-id

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const { handlePSQLErrors, handleCustomErrors, handleServerErrors, handleNonExistentEndpoints } = require('./controllers/errors-controller.js');
 const { getTopics } = require('./controllers/topics-controller.js');
 const { getApi } = require('./controllers/api-controller.js');
-const { getArticleById, getArticles } = require('./controllers/articles-controller.js');
+const { getArticleById, getArticles, patchArticlesById } = require('./controllers/articles-controller.js');
 const { getCommentsByArticleId, postCommentByArticleId } = require('./controllers/comments-controller.js');
 
 app.use(express.json());
@@ -15,6 +15,7 @@ app.get('/api/topics', getTopics);
 app.get('/api/articles', getArticles);
 
 app.get('/api/articles/:article_id', getArticleById);
+app.patch('/api/articles/:article_id', patchArticlesById);
 
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
 app.post('/api/articles/:article_id/comments', postCommentByArticleId);
