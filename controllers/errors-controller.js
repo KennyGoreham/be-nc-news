b1943ad35ebf305a63284969544fa9af4736bf28
KennyGoreham <goreham.kenny2@gmail.com>
2024-02-23 22:35:55 +0000

    fixed next being called after a response was sent

--- controllers/errors-controller.js
+++ controllers/errors-controller.js
@@ -8,12 +8,12 @@ exports.handlePSQLErrors = (err, req, res, next) => {
     if(err.code === '22P02' || err.code === '23502') {
         res.status(400).send({ msg: "Bad request." });
     }
-
-    if(err.code === '23503') {
+    else if(err.code === '23503') {
         res.status(404).send({ msg: "Resource not found." });
     }
-
-    next(err);
+    else {
+        next(err);
+    }
 }
 
 exports.handleCustomErrors = (err, req, res, next) => {
@@ -21,11 +21,12 @@ exports.handleCustomErrors = (err, req, res, next) => {
     if(err.status && err.msg) {
         res.status(err.status).send({ msg: err.msg });
     }
-
-    next(err);
+    else {
+        next(err);
+    }
 }
 
 exports.handleServerErrors = (err, req, res, next) => {
-    
+
     res.status(500).send("Internal server error.");
 }
